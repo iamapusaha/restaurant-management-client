a6195c5105e3a23aa49265aeadacbeff26ec78da
iamapusaha <apusaha392@gmail.com>
2023-11-10 15:47:06 +0600

    added footer

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,13 @@
 
 // import PropTypes from 'prop-types';
 
+import Footer from "../../Shared/Footer";
+
 const Home = () => {
     return (
         <div>
             <h1>this is home!</h1>
+            <Footer></Footer>
         </div>
     );
 };
