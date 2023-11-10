c090b6e1d524cba9cba528dcb8639572a7b74542
iamapusaha <apusaha392@gmail.com>
2023-11-10 10:15:49 +0600

    added and setup react router

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,16 @@
+
+// import PropTypes from 'prop-types';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>this is home!</h1>
+        </div>
+    );
+};
+
+Home.propTypes = {
+
+};
+
+export default Home;
