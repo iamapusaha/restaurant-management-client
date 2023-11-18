08520a5c484dc3f879c27a4eb1a4ee4bb6666093
iamapusaha <apusaha392@gmail.com>
2023-11-18 12:36:33 +0600

    added auth custom hook

--- src/components/SectionTitle/FoodCard/FoodCard.jsx
+++ src/components/SectionTitle/FoodCard/FoodCard.jsx
@@ -2,6 +2,9 @@
 
 const FoodCard = ({ item }) => {
     const { image, name, recipe, price } = item;
+    const handleAddToCart = (name) => {
+        console.log(name);
+    }
     return (
         <div className="card min-w-96 bg-base-100 shadow-xl">
             <figure className="">
@@ -12,7 +15,7 @@ const FoodCard = ({ item }) => {
                 <h2 className="card-title">{name}</h2>
                 <p>{recipe}</p>
                 <div className="card-actions">
-                    <button className="btn btn-outline border-0 border-b-4 bg-slate-100 border-orange-400">Buy Now</button>
+                    <button onClick={() => handleAddToCart(item)} className="btn btn-outline border-0 border-b-4 bg-slate-100 border-orange-400">Buy Now</button>
                 </div>
             </div>
         </div>
