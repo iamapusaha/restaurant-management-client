21453a4220499e49fe143aadb36e8f85d79f7195
iamapusaha <apusaha392@gmail.com>
2023-11-20 15:46:54 +0600

    display all cart data

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -8,10 +8,50 @@ const Cart = () => {
     return (
         <div>
             <SectionTitle header='WANNA ADD MORE?' subHeader='My Cart'></SectionTitle>
-            <div className="flex justify-evenly">
+            <div className="flex justify-evenly my-8">
                 <h1 className="text-3xl font-bold uppercase">Total orders: {cart.length}</h1>
                 <h1 className="text-3xl font-bold uppercase">Total orders: {totalPrice}</h1>
                 <button className="btn btn-primary">Pay</button>
+            </div>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>ITEM IMAGE</th>
+                            <th>ITEM NAME</th>
+                            <th>PRICE</th>
+                            <th>ACTION</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            cart.map((item, idx) => <tr key={item._id}>
+                                <th>{idx + 1}</th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={item.image} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    {item.name}
+                                </td>
+                                <td>{item.price}</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </th>
+                            </tr>)
+                        }
+
+                    </tbody>
+
+                </table>
             </div>
         </div>
     );
