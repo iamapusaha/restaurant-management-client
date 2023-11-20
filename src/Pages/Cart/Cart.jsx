afb8a0d2b20eff37f34db5dd969c2ad23696fa4f
iamapusaha <apusaha392@gmail.com>
2023-11-20 14:52:52 +0600

    addded top section on cart page

--- src/Pages/Cart/Cart.jsx
+++ src/Pages/Cart/Cart.jsx
@@ -1,10 +1,18 @@
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
+import useCart from "../../hooks/useCart";
 
 
 const Cart = () => {
+    const [cart] = useCart();
+    const totalPrice = cart.reduce((acc, item) => acc + item.price, 0)
     return (
         <div>
             <SectionTitle header='WANNA ADD MORE?' subHeader='My Cart'></SectionTitle>
+            <div className="flex justify-evenly">
+                <h1 className="text-3xl font-bold uppercase">Total orders: {cart.length}</h1>
+                <h1 className="text-3xl font-bold uppercase">Total orders: {totalPrice}</h1>
+                <button className="btn btn-primary">Pay</button>
+            </div>
         </div>
     );
 };
