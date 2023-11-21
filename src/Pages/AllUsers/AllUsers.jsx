7693e3a60d608b2ae75f582bc63da8758f705bb0
iamapusaha <apusaha392@gmail.com>
2023-11-21 20:55:32 +0600

    delete user data from admin dashboard

--- src/Pages/AllUsers/AllUsers.jsx
+++ src/Pages/AllUsers/AllUsers.jsx
@@ -1,11 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 import useAxiousPublic from "../../hooks/useAxiousPublic";
+import { FaTrashAlt } from "react-icons/fa";
+import useAxiousSecure from "../../hooks/useAxiousSecure";
+import Swal from "sweetalert2";
 
 
 const AllUsers = () => {
-    const axiosPublic = useAxiousPublic()
-    const { data: users = [] } = useQuery({
+    const axiosPublic = useAxiousPublic();
+    const axiosSecure = useAxiousSecure()
+    const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosPublic.get('/users')
@@ -13,6 +17,32 @@ const AllUsers = () => {
         }
 
     })
+    const handleDeleteUser = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                axiosSecure.delete(`/users/${id}`)
+                    .then(res => {
+                        console.log(res);
+                        if (res.data.deletedCount > 0) {
+                            refetch()
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your file has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+    }
     return (
         <div>
             <SectionTitle header={'MANAGE ALL USERS'} subHeader={'ALL USERS'}></SectionTitle>
@@ -38,7 +68,11 @@ const AllUsers = () => {
                                 <td>{user.name}</td>
                                 <td>{user.email}</td>
                                 <td>Role</td>
-                                <td>details</td>
+                                <td>
+                                    <button onClick={() => handleDeleteUser(user._id)} className="btn btn-ghost btn-lg">
+                                        <FaTrashAlt></FaTrashAlt>
+                                    </button>
+                                </td>
                             </tr>)
                         }
 
