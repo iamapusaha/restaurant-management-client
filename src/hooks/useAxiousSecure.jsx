ba081c961591189fbb5f3704ee0280a78736a034
iamapusaha <apusaha392@gmail.com>
2023-11-22 13:26:18 +0600

    check response status for 403 and 401

--- src/hooks/useAxiousSecure.jsx
+++ src/hooks/useAxiousSecure.jsx
@@ -1,19 +1,36 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 export const axiosSecure = axios.create({
     baseURL: 'http://localhost:5000'
 })
 const useAxiousSecure = () => {
+    const { logOut } = useAuth();
+    const navigate = useNavigate()
+    // interceptors for request
     axiosSecure.interceptors.request.use(function (config) {
 
         const token = localStorage.getItem('access-token');
-        console.log('request stoped by intersceptors', token);
         config.headers.authorization = `Bearer ${token}`
         return config;
     }, function (error) {
         // Do something with request error
         return Promise.reject(error);
     })
+    //interceptors for response
+    axiosSecure.interceptors.response.use(function (response) {
+        return response;
+    }, async (error) => {
+        const status = error.response.status;
+        console.log(status);
+        if (status === 401 || status === 403) {
+            await logOut()
+            navigate('/login')
+        }
+        return Promise.reject(error);
+    })
+
     return axiosSecure;
 };
 
